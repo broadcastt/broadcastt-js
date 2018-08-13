1f6ef6fbf912e1fb1f57b480a941961120e6b37e
Nerg4l <gorog_laci@hotmail.com>
2018-08-13 20:32:09 +0200

    Fixing bugs
    Added underline before protected attributes.
    Replaced all quotes to single quotes.
    Fixed subscribe bug.
    Fixed unsubscribe bug.
    Changed event binding.

--- src/presence-channel.ts
+++ src/presence-channel.ts
@@ -1,4 +1,4 @@
-import PrivateChannel from "./private-channel";
+import PrivateChannel from './private-channel';
 
 class Members {
     public me: {user_id: String, user_info: any};
@@ -9,72 +9,91 @@ class Members {
 
 export default class PresenceChannel extends PrivateChannel {
 
-    protected onhere: (members) => any;
-    protected onjoin: (e, members) => any;
-    protected onleave: (e, members) => any;
     public members: Members;
 
-    constructor(channel, connection, auth_endpoint) {
-        super(channel, connection, auth_endpoint);
+    constructor(channel, shared) {
+        super(channel, shared);
 
         this.members = new Members();
-
-        this.onhere = () => {};
-        this.onjoin = () => {};
-        this.onleave = () => {};
     }
 
     protected registerDefaultListeners() {
-        this.listeners.push({
+        this._listeners.push({
             event: 'broadcastt_internal:subscription_succeeded',
             callback: (e) => {
-                this.status = "subscribed";
+                this._status = 'subscribed';
                 this.members = Object.assign({}, this.members, e.presence);
-                this.onhere(this.members);
+
+                this.emit('broadcastt:subscription_succeeded', this.members);
             },
         });
 
-        this.listeners.push({
+        this._listeners.push({
             event: 'broadcastt_internal:member_added',
             callback: (e) => {
                 this.members.count++;
                 this.members.ids.push(e.user_id);
                 this.members.hash[e.user_id] = e.user_info;
 
-                this.onjoin(Object.assign({}, e), this.members);
+                this.emit('broadcastt:member_added', Object.assign({}, e), this.members);
             },
         });
 
-        this.listeners.push({
+        this._listeners.push({
             event: 'broadcastt_internal:member_removed',
             callback: (e) => {
                 this.members.ids = this.members.ids.filter((i) => i !== e.user_id);
                 delete this.members.hash[e.user_id];
 
-                this.onleave(Object.assign({}, e), this.members);
+                this.emit('broadcastt:member_removed', Object.assign({}, e), this.members);
             },
         });
     }
 
     protected onAjaxSuccess(response) {
         this.members.me = response;
+
         super.onAjaxSuccess(response);
     }
 
+    /**
+     * Binds a callback to the `broadcastt:subscription_succeeded` event
+     *
+     * This event is called when the subscription succeeds.
+     * The defined callback will be called with the data of the connected clients.
+     *
+     * @param callback
+     */
     public here(callback: (members) => any) {
-        this.onhere = callback;
+        this.bind('broadcastt:subscription_succeeded', callback);
 
         return this;
     }
 
+    /**
+     * Binds a callback to the `broadcastt:member_added` event
+     *
+     * This event is called when a new clients connects to the channel.
+     * The defined callback will be called with the data of that clients.
+     *
+     * @param callback
+     */
     public joining(callback: (e, members) => any) {
-        this.onjoin = callback;
+        this.bind('broadcastt:member_added', callback);
 
         return this;
     }
 
+    /**
+     * Binds a callback to the `broadcastt:member_removed` event
+     *
+     * This event is called when a clients disconnects from the channel.
+     * The defined callback will be called with the id of that clients.
+     *
+     * @param callback
+     */
     public leaving(callback: (e, members) => any) {
-        this.onleave = callback;
+        this.bind('broadcastt:member_removed', callback);
 
         return this;
     }
