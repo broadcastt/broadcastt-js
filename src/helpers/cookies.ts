9976030e59d05c4e38ded0d095a0926841549e5a
László Görög <contact@nergal.xyz>
2019-09-19 14:26:45 +0200

    Add support for XSRF token

---
+++ src/helpers/cookies.ts
@@ -0,0 +1,6 @@
+export default class Cookies {
+    public read(name: string): string {
+        const match = document.cookie.match(new RegExp('(^|;\\s*)(' + name + ')=([^;]*)'));
+        return (match ? decodeURIComponent(match[3]) : null);
+    }
+}
